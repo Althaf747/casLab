e680e7727db0c29378dd531841aa01aab5034873
Althaf Rizqullah <althaf8747@gmail.com>
2024-12-07 17:45:55 +0700

    .

--- src/web/web.js
+++ src/web/web.js
@@ -1,7 +1,14 @@
 import express from "express";
 
 import {tpRoute} from "../route/tpRoute.js";
+import path from "path"
+
 
 export const web = express()
 web.use(express.json())
+
+web.get('/', function(req, res) {
+    res.sendFile(__dirname + '../public/index.html');
+});
+
 web.use(tpRoute)
